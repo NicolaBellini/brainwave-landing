63f4597f2e180d8d7806475d808089d792dd8989
NicolaBellini <Bellininicola4366@gmail.com>
2024-11-29 15:18:04 +0100

    [add] menu lg

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,14 +5,23 @@ const Button = ({ className, href, onClick, children, px, white }) => {
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
 
+  const spanClasses = "relative z-10";
+
   const rederButton = () => (
     <button className={classes}>
-      <span>{children}</span>
+      <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
   );
 
-  return rederButton();
+  const renderLink = () => (
+    <a href={href} className={classes}>
+      <span className={spanClasses}>{children}</span>
+      {ButtonSvg(white)}
+    </a>
+  );
+
+  return href ? renderLink() : rederButton();
 };
 
 export default Button;
